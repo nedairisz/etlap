bc1f6463f5eb56090141452a95242b9f58fa703b
nedairisz <iriszneda@gmail.com>
2023-10-11 13:49:34 +0200

    rendelés összeállítása

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import etlap
-import rendeles
 
 etlap_meret = 60
 
@@ -7,15 +6,18 @@ etlap.cim("ÉTLAP", etlap_meret)
 
 etlap.szoveg_kiiras("*", "Levesek", "*", etlap_meret)
 
-etlap.megnevezes_osszeg("*", "Gulyás leves", "1800 Ft", "*", etlap_meret)
-etlap.megnevezes_osszeg("*", "Tojás leves", "1400 Ft", "*", etlap_meret)
+etlap.megnevezes_osszeg("*", "1. Gulyás leves", "1800 Ft", "*", etlap_meret)
+etlap.megnevezes_osszeg("*", "2. Tojás leves", "1400 Ft", "*", etlap_meret)
 
 etlap.szoveg_kiiras("*", "Főételek", "*", etlap_meret)
 
-etlap.megnevezes_osszeg("*", "Lasagne", "2000 Ft", "*", etlap_meret)
-etlap.megnevezes_osszeg("*", "Rizottó", "1800 Ft", "*", etlap_meret)
+etlap.megnevezes_osszeg("*", "1. Lasagne", "2000 Ft", "*", etlap_meret)
+etlap.megnevezes_osszeg("*", "2. Rizottó", "1800 Ft", "*", etlap_meret)
 
 etlap.cim("JÓ ÉTVÁGYAT!", etlap_meret)
 
+import rendeles
+rendeles.rendeles("1. Gulyás leves", "1800 Ft", "2. Tojás leves", "1400 Ft", "1. Lasagne", "2000 Ft", "2. Rizottó", "1800 Ft")
 
-kerdes1: str = input("Kér levest (I/N)? ")
+
+
--- rendeles.py
+++ rendeles.py
@@ -1,21 +1,31 @@
-def renedeles():
+import szamla
+def rendeles(l1, l1_ar, l2, l2_ar, f1, f1_ar, f2, f2_ar):
 
     valasztott_leves=""
     valasztott_foetel=""
+    v_l_ar =0
+    v_f_ar =0
 
     kerdes1: str = input("Kér levest I/N ? ")
-    if kerdes1 == "I" or "i" or "igen" or "Igen":
+    if kerdes1 == "I" or kerdes1 == "i" or kerdes1 == "igen" or kerdes1 == "Igen":
         valasz1: str = input("Melyik levest kéri 1/2 ? ")
         if valasz1 == "1":
-            valasztott_leves = "Gulyás leves"
-        else:
-            valasztott_leves = "Tojás leves"
+            valasztott_leves = l1
+            v_l_ar = l1_ar
+        elif valasz1 == "2":
+            valasztott_leves = l2
+            v_l_ar = l2_ar
 
-    elif kerdes1 == "N" or "n" or "nem" or "Nem":
-        kerdes2: str = input("Kér főételt I/N ? ")
-        if kerdes2 == "I" or "i" or "igen" or "Igen":
-            valasz2: str = input("Melyik főételt kéri 1/2 ? ")
-            if valasz2 == "1":
-                valasztott_foetel = "Lasagne"
-            else:
-                valasztott_foetel = "Rizottó"
+
+    kerdes2: str = input("Kér főételt I/N ? ")
+    if kerdes2 == "I" or "i" or "igen" or "Igen":
+        valasz2: str = input("Melyik főételt kéri 1/2 ? ")
+        if valasz2 == "1":
+            valasztott_foetel = f1
+            v_f_ar = f1_ar
+        elif valasz2 == "2":
+            valasztott_foetel = f2
+            v_f_ar = f2_ar
+
+
+
